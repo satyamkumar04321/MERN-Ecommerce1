0590042a5b60c01fb2e864aa9f686c3114f4476f
satyamkumar04321 <satyamkumar04321@gmail.com>
2026-08-04 15:09:48 +0530

    fix: completely remove BackendWaker loading screen

--- frontend/src/components/layout/Server-Health/BackendWaker.js
+++ frontend/src/components/layout/Server-Health/BackendWaker.js
@@ -1,40 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { wakeUpServer } from '../../../actions/serverAction';
-import './BackendWaker.css';
-import logo from '../../../images/Ecommerce-logo.png';
+import React from 'react';
 
 const BackendWaker = ({ children }) => {
-  const dispatch = useDispatch();
-  const { isAwake } = useSelector(state => state.server);
-  const [dots, setDots] = useState('');
-
-  useEffect(() => {
-    dispatch(wakeUpServer());
-
-    const interval = setInterval(() => {
-      setDots(prev => (prev.length < 3 ? prev + '.' : ''));
-    }, 500);
-    return () => clearInterval(interval);
-  }, [dispatch]);
-
-  if (!isAwake) {
-    return (
-      <div className="waker-overlay">
-        <div className="waker-content">
-          <div className="logo-placeholder">
-            <img src={logo} alt="Ecommerce Logo" className="waker-logo" />
-          </div>
-          <h1 className="waker-title">Order Planning</h1>
-          <p className="waker-status">Connecting to server{dots}</p>
-          <div className="progress-container">
-            <div className="progress-bar"></div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   return children;
 };
 
